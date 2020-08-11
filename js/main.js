da12e1f973c2ffb293b6d5013218d811d867ae65
oseeshogun <omasuaku@gmail.com>
2020-08-11 15:06:32 +0100

    adding demo & putting prices

--- js/main.js
+++ js/main.js
@@ -121,3 +121,68 @@
 
 })(jQuery);
 
+
+const sizes = document.querySelectorAll('#live-demo .size');
+const colors = document.querySelectorAll('#live-demo .color');
+const shoes = document.querySelectorAll('#live-demo .shoe');
+const gradients = document.querySelectorAll('#live-demo .gradient');
+const shoeBg = document.querySelector('#live-demo .shoeBackground');
+
+let prevColor = "blue";
+let animationEnd = true;
+
+function changeSize(){
+    sizes.forEach(size => size.classList.remove('active'));
+    this.classList.add('active');
+}
+
+function changeColor(){
+    if(!animationEnd) return;
+    let primary = this.getAttribute('primary');
+    let color = this.getAttribute('color');
+
+    let shoe = document.querySelector('#live-demo .shoe[color="'+color+'"]');
+    console.log(shoe, color, primary);
+    let gradient = document.querySelector('#live-demo .gradient[color="'+color+'"]');
+    let prevGradient = document.querySelector('#live-demo .gradient[color="'+prevColor+'"]');
+
+    if(color == prevColor) return;
+
+    colors.forEach(c => c.classList.remove('active'));
+    this.classList.add('active');
+
+    document.documentElement.style.setProperty('--primary', primary);
+    
+    shoes.forEach(s => s.classList.remove('show'));
+    shoe.classList.add('show');
+
+    gradients.forEach(g => g.classList.remove('first', 'second'));
+    gradient.classList.add('first');
+    prevGradient.classList.add('second');
+
+    prevColor = color;
+    animationEnd = false;
+
+    gradient.addEventListener('animationend', () => {
+        animationEnd = true;
+    })
+}
+
+sizes.forEach(size => size.addEventListener('click', changeSize));
+colors.forEach(c => c.addEventListener('click', changeColor));
+
+let x = window.matchMedia("(max-width: 1000px)");
+
+function changeHeight(){
+    if(x.matches){
+        let shoeHeight = shoes[0].offsetHeight;
+        shoeBg.style.height = `${shoeHeight * 0.9}px`;
+    }
+    else{
+        shoeBg.style.height = "475px";
+    }
+}
+
+changeHeight();
+
+window.addEventListener('resize', changeHeight);
